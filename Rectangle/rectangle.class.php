25e86dbe428f63fcfc616f22480e40db0ca4fd5a
LilacLoaf <nguyenjq@iu.edu>
2025-08-28 11:39:11 -0400

    test

--- Rectangle/rectangle.class.php
+++ Rectangle/rectangle.class.php
@@ -1,21 +1,51 @@
 <?php
-/** 
-* Author: Jonathan Nguyen
-* Date: 8/28/2025
-* File: rectangle.class.php
-* Description: 
-*/?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title> </title>
-</head>
-<body>
-<?php
+/**
+ * Author: Jonathan Nguyen
+ * Date: 8/28/2025
+ * File: rectangle.class.php
+ * Description:
+ */
 //Enter your PHP code here
 
-?>
-</body>
-</html>
+class Rectangle {
+private float $width, $height;
+
+
+
+//set the rectangle's width
+public function getWidth(): float {
+    return $this->width;
+}
+
+//set the rectangle's height
+public function getHeight(): float {
+    return $this->height;
+}
+
+//generated code
+    public function setWidth(float $width): Rectangle
+    {
+        $this->width = $width;
+        return $this;
+    }
+
+    public function setHeight(float $height): Rectangle
+    {
+        $this->height = $height;
+        return $this;
+    }
+
+
+
+//calculate area of the rectangle
+public function calculateArea(): float {
+    return $this->width * $this->height;
+}
+
+//calculate perimeter of the rectangle
+public function calculatePerimeter(): float {
+    return ($this->width + $this->height) * 2;
+}
+
+
+}
--- Rectangle/rectangle.php
+++ Rectangle/rectangle.php
@@ -1,20 +1,30 @@
 <?php
-/** 
-* Author: Jonathan Nguyen
-* Date: 8/28/2025
-* File: rectangle.php
-* Description: 
-*/?>
+/**
+ * Author: Jonathan Nguyen
+ * Date: 8/28/2025
+ * File: rectangle.php
+ * Description:
+ */ ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title> </title>
+    <meta charset="UTF-8">
+    <title>Test the rectangle class</title>
 </head>
 <body>
 <?php
 //Enter your PHP code here
+require_once("rectangle.class.php");
+
+$r1 = new Rectangle();
+$r2 = new Rectangle();
+
+$r1 -> setWidth(width:20)->setHeight(height:30);
+$area1 = $r1->calculateArea();
+$perimeter1 = $r1->calculatePerimeter();
+
+echo "Rectangle 1: Area = $area1. Perimeter = $perimeter1 <br>";
 
 ?>
 </body>
