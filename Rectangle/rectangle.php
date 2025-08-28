77500a4fbd865ef1419857edd3404bc7ab7b40e1
LilacLoaf <nguyenjq@iu.edu>
2025-08-28 11:35:29 -0400

    Initial commit

---
+++ Rectangle/rectangle.php
@@ -0,0 +1,21 @@
+<?php
+/** 
+* Author: Jonathan Nguyen
+* Date: 8/28/2025
+* File: rectangle.php
+* Description: 
+*/?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title> </title>
+</head>
+<body>
+<?php
+//Enter your PHP code here
+
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Author: Jonathan Nguyen
+ * Date: 8/28/2025
+ * File: index.php
+ * Description:
+ */ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+//Enter your PHP code here
+echo "test";
+?>
+</body>
+</html>
