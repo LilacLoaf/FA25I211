eced28784d80d091bcb66b9b1df7a1ff4899bf60
Loaf <nguyenjq@iu.edu>
2025-10-09 21:44:23 -0400

    lab 7

--- Labs/Lab06/pyramid_do.php
+++ Labs/Lab06/pyramid_do.php
@@ -5,3 +5,17 @@
  * File: pyramid_do.php
  * Description:
  */
+
+//get the pyramid class
+require_once "pyramid.class.php";
+//recieve the width length and height
+$width = $_GET["width"];
+$length = $_GET["length"];
+$height = $_GET["height"];
+//create the new object
+$pyramid = new pyramid($width, $length, $height);
+//run the tostring function
+$output = $pyramid->toString();
+
+//send the json back
+echo json_encode($output);
